874481c6471ced5228ef5dbbd9278058633910b2
Md Al Amin <fpalamin6@gmail.com>
2022-10-25 19:22:10 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1 className='text-3xl font-bold'>this is education website</h1>
+      <Header></Header>
     </div>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div>
+            <div className="navbar bg-slate-700">
+  <div className="flex-1">
+    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+  </div>
+  <div className="flex-none">
+    <ul className="menu menu-horizontal p-0">
+      <li><Link>Course</Link></li>
+      <li><Link>FAQ</Link></li>
+      <li><Link>Course</Link></li>
+      
+    </ul>
+  </div>
+</div>
+        </div>
+    );
+};
+
+export default Header;
