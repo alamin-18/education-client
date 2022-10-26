24e4afd5279b34821eeca9c7591738f854608907
Md Al Amin <fpalamin6@gmail.com>
2022-10-26 11:56:33 +0600

    register added

--- src/Contexts/UserContext/UserContext.jsx
+++ src/Contexts/UserContext/UserContext.jsx
@@ -1,10 +1,20 @@
 import React, { useState } from 'react';
 import { createContext } from 'react';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { app } from '../../firebase/firebase.config';
 
+
+const auth = getAuth(app)
 export const AuthContext = createContext()
 const UserContext = ({children}) => {
+    //user state
     const [user,setUser] = useState({name:"alamin",age:42})
-    const AuthInfo ={user}
+
+    //create user
+    const register = (email,password) =>{
+       return createUserWithEmailAndPassword(auth,email,password)
+    }
+    const AuthInfo ={user,register}
     return (
         <AuthContext.Provider value={AuthInfo}>
             {children}
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../Contexts/UserContext/UserContext';
 
 const Register = () => {
+    const {register} = useContext(AuthContext)
+    const handleSubmit = event =>{
+        event.preventDefault();
+        const form = event.target
+        const name = form.name.value
+        const photoUrl = form.photoUrl.value
+        const email = form.email.value
+        const password = form.password.value
+
+        register(email,password)
+        .then( (result =>{
+            const user = result.user
+            console.log(user)
+        }))
+        .catch((error) => {
+            console.error(error)
+            // ..
+          });
+        // console.log(name,photoUrl,email,password)
+
+    }
     return (
         <div>
              <div className="hero min-h-screen bg-base-200">
@@ -11,6 +33,7 @@ const Register = () => {
                         <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                     </div>
                     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <form onSubmit={handleSubmit}>
                         <div className="card-body">
                             <div className="form-control">
                                 <label className="label">
@@ -22,7 +45,7 @@ const Register = () => {
                                 <label className="label">
                                     <span className="label-text">Photo Url</span>
                                 </label>
-                                <input type="text" placeholder="Photo url" className="input input-bordered" name='photo-url' />
+                                <input type="text" placeholder="Photo url" className="input input-bordered" name='photoUrl' />
                             </div>
                             <div className="form-control">
                                 <label className="label">
@@ -36,13 +59,14 @@ const Register = () => {
                                 </label>
                                 <input type="password" placeholder="Password" className="input input-bordered" name='password' />
                                 <label className="label">
-                                    <Link to='/register' className="label-text-alt link link-hover text-violet-600">Already have a account Please Login?</Link>
+                                    <Link to='/logIn' className="label-text-alt link link-hover text-violet-600">Already have a account Please Login?</Link>
                                 </label>
                             </div>
                             <div className="form-control mt-6">
                                 <button className="btn btn-primary">Sing Up</button>
                             </div>
                         </div>
+                        </form>
                     </div>
                 </div>
             </div>
